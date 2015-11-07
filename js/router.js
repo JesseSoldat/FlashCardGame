258355937b92b0ce9d8164b3446d062c67fe0b25
Jesse Soldat <jsoldat@hotmail.com>
2015-11-06 22:26:22 -0500

    installed cookies backbone react

---
+++ js/router.js
@@ -0,0 +1,6 @@
+import Backbone from 'backbone';
+import React from 'react';
+import ReactDom from 'react-dom';
+import _ from 'underscore';
+import Cookies from 'js-cookie';
+//----------------------------
